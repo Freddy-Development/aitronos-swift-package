c94add3cc7b782711d1397402c2ad03f4736685a
phillipLoacker <124287196+phillipLoacker@users.noreply.github.com>
2024-09-23 11:23:47 +0200

    upload

--- Tests/aitronos-swift-packageTests/aitronos_swift_packageTests.swift
+++ Tests/aitronos-swift-packageTests/aitronos_swift_packageTests.swift
@@ -2,5 +2,5 @@ import Testing
 @testable import aitronos_swift_package
 
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    print("Hello, World!")
 }
